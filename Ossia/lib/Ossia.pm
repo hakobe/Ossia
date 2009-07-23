c3885765945c9c34360ae008f8ae02c3a109f2d1
hakobe932 <hakobe@gmail.com>
2009-07-23 17:56:43 +0900

    initial import

---
+++ Ossia/lib/Ossia.pm
@@ -0,0 +1,35 @@
+package Ossia;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Ossia -
+
+=head1 SYNOPSIS
+
+  use Ossia;
+
+=head1 DESCRIPTION
+
+Ossia is
+
+=head1 AUTHOR
+
+Yohei Fushii E<lt>hakobe@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
