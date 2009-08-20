62598b060e1c402d53c122d9d8a9a4bbadb42537
hakobe932 <hakobe@gmail.com>
2009-08-20 22:03:12 +0900

    static path を扱えるように
    chord から借りた

--- lib/Ossia/Smart.pm
+++ lib/Ossia/Smart.pm
@@ -4,9 +4,11 @@ use Any::Moose;
 extends qw(Ossia);
 
 use Exporter::Lite;
-our @EXPORT = qw(get post);
+our @EXPORT = qw(get post static);
 
 use List::MoreUtils qw(zip);
+use HTTP::Engine;
+use HTTP::Engine::Middleware;
 
 no Any::Moose;
 
@@ -21,7 +23,7 @@ sub instance {
 sub register_smart_action {
     my ($self, $method, $path, $handler) = @_;
 
-    my @capture_keys;
+    my @capture_keys = ();
     my $pattern = "$path";
     $pattern =~ s{:([^:]+)}{
         push @capture_keys, $1;
@@ -53,6 +55,17 @@ sub post {
     $self->register_smart_action( 'POST', $path, $handler);
 }
 
+my $mw = HTTP::Engine::Middleware->new;
+sub static {
+	my ($path, $base) = @_;
+    return unless $path;
+    $path =~ s{^/+}{}xms;
+    $mw->install('HTTP::Engine::Middleware::Static' => {
+        regexp => qr{^ / $path / .+ }xms,
+        docroot => $base || 'static',
+    });
+}
+
 sub run {
     my ($class, %opts) = @_;
 
@@ -64,7 +77,9 @@ sub run {
                 host => 'localhost',
                 port =>  3000,
             },
-            request_handler => $self->handler,
+            request_handler => $mw->handler(sub {
+                $self->handler->(@_);
+            }),
             %opts
         },
     )->run;
